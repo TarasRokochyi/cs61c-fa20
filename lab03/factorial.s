f3ff75b4d73a672ab11983088056d9d9d38baeae
TarasRokochyi <tarrok2005@gmail.com>
2024-01-27 19:34:11 +0200

    lab03

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,26 @@ main:
 
 factorial:
     # YOUR CODE HERE
+
+    addi sp, sp, -8   # allocate memory for ra and current argument (a0)
+    sw ra, 0(sp)
+    sw a0, 4(sp)
+
+    addi a0, a0, -1    
+
+    bne a0, x0, calc   # if n is 0, return 1
+    lw a0, 4(sp)      # recover a0
+    addi sp, sp, 8
+    ret
+
+calc:
+
+    jal factorial    # call another factorial
+
+    lw t0, 4(sp)     # recover saved registers
+    lw ra, 0(sp)
+    addi sp, sp, 8
+
+    mul a0, a0, t0   # multiply return value with current argument
+
+    ret
